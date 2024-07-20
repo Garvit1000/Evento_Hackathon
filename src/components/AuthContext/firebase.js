39d7a58ec3d60dbb07a48bdd2bed60b10f25cb2c
Garvit Joshi <163440488+Garvit1000@users.noreply.github.com>
2024-07-20 16:12:58 +0530

    Update firebase.js

--- src/components/AuthContext/firebase.js
+++ src/components/AuthContext/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth';
 
 //  Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCX8BMv9hQX6-laaNP2Mdvu_wjf9tCz-XE",
-    authDomain: "evento-3655e.firebaseapp.com",
-    projectId: "evento-3655e",
-    storageBucket: "evento-3655e.appspot.com",
-    messagingSenderId: "1045833272948",
-    appId: "1:1045833272948:web:495d72f9d405e484104b21"
+   apiKey: "YOUR API KEY",
+    authDomain: "YOUR AUTHDOMAIN",
+    projectId: "YOUR PROJECT ID",
+    storageBucket: "YOUR STORAGE BUCKET",
+    messagingSenderId: "YOUR MESSAGE SENDER ID",
+    appId: "YOUR APP ID"
 };
 
 // Initialize Firebase
